fc641acd937d0b2edef4e0438c9fa907ec1e4b12
M.Sakurai <thesakumasan@gmail.com>
2019-08-27 16:55:24 +0900

    20190827_phpTwitterBot_simple

---
+++ twitter-bot.php
@@ -0,0 +1,38 @@
+<?php
+// OAuthスクリプトの読み込み
+require "vendor/autoload.php";
+use Abraham\TwitterOAuth\TwitterOAuth;
+
+// Consumer key
+$cosumer_key = "fFo7i2mbcXaoM4ycimzmh6s8y";
+// Consumer secret
+$cosumer_secret = "lHcCROz5AGsKQDmNa9IZHIsUm4E0iLakxEh6BOOt5hcaYvuji3";
+// Access token
+$access_token = "1166169015313502208-QrFuDdzOgI5mG8SFZHZHkPQI4VIpsu";
+// Access token secret
+$access_token_secret = "E3mm0EFw0DoQn3hwE39HNj4LvxYFhGIw38auQLTbgGc4M";
+
+// おまじない
+$header = 'Content-Type: text/plain; charset=utf-8';
+
+// つぶやく
+
+try {
+  // TwistOAuthの新しい接続を生成
+  $connection = new TwitterOAuth($cosumer_key, $cosumer_secret, $access_token, $access_token_secret);
+  // ツイートする
+  $res = $connection->post('statuses/update', [
+      'status' => 'こんにちは'
+  ]);
+  // おまじない
+  header($header, true, 200);
+  // 返却値。成功したら、「ツイートしました。」とブラウザに表示
+  echo "ツイートしました";
+} catch (Exception $e) {
+  // おまじない
+  header($header, true, $e->getCode() ?: 500);
+  // ツイートに失敗したら、「ツイート失敗」と表示
+  echo "ツイート失敗: {$e->getMessage()}\n";
+}
+
+?>
